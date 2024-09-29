58acdfb7ae6a750f4e5c0fe0e290593c751c2170
wangqiang <670248996@qq.com>
2024-09-29 17:20:51 +0800

    添加xxl-job

---
+++ pandama-module-job/pandama-module-job-service/src/main/java/com/pandama/top/job/JobApplication.java
@@ -0,0 +1,19 @@
+package com.pandama.top.job;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 定时任务
+ *
+ * @author xuxueli 2018-10-28 00:38:13
+ * @date 2024-09-29 17:19:59
+ */
+@SpringBootApplication
+public class JobApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(JobApplication.class, args);
+	}
+
+}
